1ecf90597f34b9ef2caaec46acff488f23e2fde3
Yura Kocyuba <yurakocyuba@yandex.ru>
2018-12-03 18:09:56 +0200

    add endpoint get_actions

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from elasticsearch import Elasticsearch
 app = Flask(__name__)
 
 
-client = Elasticsearch([{'host': '127.0.0.1', 'port': '9200'}])
-
+client = Elasticsearch([{'host': '10.0.64.25', 'port': '9200'}])
 
 @app.route('/v1/history/get_transaction', methods=['POST'])
 def get_transaction():
@@ -15,14 +14,12 @@ def get_transaction():
     if len(transaction_id) != 64:
         return abort(404)
 
-
     seeking_result = seeking_transaction(transaction_id)
 
     if seeking_result == None:
         return abort(404)
 
     return jsonify(seeking_result)
-
 
 def seeking_transaction(transaction_id):
     resp = client.search(index='transaction_traces', body={
@@ -49,8 +46,53 @@ def seeking_transaction(transaction_id):
                   'net_usage': field['_source']['net_usage'],
         }
 
+    return result
+
+@app.route('/v1/history/get_actions', methods=['POST'])
+def get_actions():
+
+    pos = request.get_json(force=True).get('pos')
+
+    offset = request.get_json(force=True).get('offset')
+
+    account_name =request.get_json(force=True).get('account_name')
+
+    if not isinstance(pos, int) or not isinstance(offset, int):
+        return abort(404)
+
+    seeking_result = seeking_actions(pos, offset, account_name)
+
+    if seeking_result == None:
+        return abort(404)
+
+    return jsonify(seeking_result)
+
+
+
+
+def seeking_actions(pos, offset, account_name):
+    resp = client.search(index='action_traces', filter_path=['hits.hits._*'],
+                         size=offset, from_=pos,
+                         body={
+                             "query":
+                                 {"match":
+                                      {"act.name": account_name
+                                       }
+                                  },
+                             "sort": [
+                                 {"block_num": {"order": "desc"}}
+                             ]
+                         })
+    if len(resp) == 0:
+        return None
+
+    result = []
+
+    for field in resp['hits']['hits']:
+        result.append(field['_source'])
 
     return result
+
 
 
 
