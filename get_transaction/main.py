bbccc6a7cbfbf23c877f5cadde854fa97dc7b198
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-01-01 23:25:19 +0200

    add filter to get_actions

--- get_transaction/main.py
+++ get_transaction/main.py
@@ -11,7 +11,7 @@ ELASTIC_PORT = os.environ['ELASTIC_PORT']
 
 client = Elasticsearch([{'host': ELASTIC_HOST, 'port': ELASTIC_PORT}], timeout=30)
 
-
+@app.route('/v1/history/get_transaction', methods=['POST'])
 @app.route('/v2/history/get_transaction', methods=['POST'])
 def get_transaction():
 
