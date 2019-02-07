353f15cab3dd9bc0d330c553a892bd8eb321766b
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-02-07 12:33:26 +0200

    get_actions and find_actions improvements
    add filtering for last parameter

--- get_actions/main.py
+++ get_actions/main.py
@@ -204,7 +204,7 @@ def seeking_actions_to_from(account_name, from_date, to_date, es_index):
 
 def seeking_actions_last(account_name,last,es_index):
 
-    if re.fullmatch(r"([0-9]+)([y,M,w,d,h,H,m,s])", last) != True:
+    if re.fullmatch(r"([0-9]+)([y,M,w,d,h,H,m,s])", last) == None:
         return None
 
     resp = client.search(index=es_index, filter_path=['hits.hits._*'], size = 10000,
